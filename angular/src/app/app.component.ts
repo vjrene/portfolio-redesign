07f6393e272f1e1b1f23d82e297f1f5fd8a08ec3
VeeJay Are <Parallel@Vanessas-MBP.home>
2017-10-27 13:52:00 -0400

    Add in initial Angular4 code. Code for homepage svgs

---
+++ angular/src/app/app.component.ts
@@ -0,0 +1,14 @@
+import { HashLocationStrategy, Location, LocationStrategy} from '@angular/common';
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  providers: [Location, {provide: LocationStrategy, useClass: HashLocationStrategy}],
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  location: Location;
+  constructor(location: Location) { this.location = location; }
+  title = 'app';
+}
---
+++ angular/src/app/app.module.ts
@@ -0,0 +1,38 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppComponent } from './app.component';
+
+import { RouterModule, Routes } from '@angular/router';
+import { WorkComponent } from './work/work.component';
+import { AboutComponent } from './about/about.component';
+import { ContactComponent } from './contact/contact.component';
+import { ProjectComponent } from './project/project.component';
+import { HomeComponent } from './home/home.component';
+
+export const ROUTES: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'about', component: AboutComponent },
+  { path: 'work', component: WorkComponent },
+  { path: 'contact', component: ContactComponent },
+  { path: 'project', component: ProjectComponent },
+
+];
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    WorkComponent,
+    AboutComponent,
+    ContactComponent,
+    ProjectComponent,
+    HomeComponent
+  ],
+  imports: [
+    BrowserModule,
+    RouterModule.forRoot(ROUTES, { useHash: true })
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
